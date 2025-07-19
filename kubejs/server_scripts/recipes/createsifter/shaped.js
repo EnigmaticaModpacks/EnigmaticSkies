26f10b3a4b0fe6d011d44a142beda99a7bf0f4b2
MuteTiefling <mutetiefling@gmail.com>
2025-07-18 22:20:10 -0400

    sifter

--- kubejs/server_scripts/recipes/createsifter/shaped.js
+++ kubejs/server_scripts/recipes/createsifter/shaped.js
@@ -2,18 +2,18 @@ ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:createsifter/shaped/';
 
     const recipes = [
-        // {
-        //     output: `createsifter:sifter`,
-        //     pattern: ['ABA', 'CDC', 'EEE'],
-        //     key: {
-        //         A: 'aether:skyroot_planks',
-        //         B: 'actuallyadditions:wood_casing',
-        //         C: 'aether:skyroot_stick',
-        //         D: 'create:cogwheel',
-        //         E: 'aether:holystone_bricks'
-        //     },
-        //     id: `createsifter:crafting/sifter`
-        // },
+        {
+            output: `createsifter:sifter`,
+            pattern: ['ABA', 'CDC', 'EEE'],
+            key: {
+                A: 'aether:skyroot_planks',
+                B: 'create:andesite_casing',
+                C: 'aether:skyroot_stick',
+                D: 'create:cogwheel',
+                E: 'aether:holystone_bricks'
+            },
+            id: `createsifter:crafting/sifter`
+        },
         {
             output: `createsifter:string_mesh`,
             pattern: ['AAA', 'ABA', 'AAA'],
