1374176fdf43d094c9f485b580a0a293bc04e1ca
MuteTiefling <mutetiefling@gmail.com>
2025-07-20 11:12:08 -0400

    Update mana_regen.js

--- kubejs/server_scripts/player_events/mana_regen.js
+++ kubejs/server_scripts/player_events/mana_regen.js
@@ -1,9 +1,9 @@
 PlayerEvents.tick((event) => {
     const player = event.player;
-    const pData = player.persistentData;
+    // const pData = player.persistentData;
 
-    if (player.isFake() || !player.isPlayer()) return;
-    if (player.tickCount % 20 != 0) return;
+    if (player.tickCount % 20 != 0 || player.isFake() || !player.isPlayer()) return;
+    // if (player.tickCount % 20 != 0) return;
 
     let effects = player.potionEffects.getActive();
 
@@ -11,7 +11,7 @@ PlayerEvents.tick((event) => {
         // console.log(effect.getDescriptionId());
         // console.log(effect.getAmplifier());
         if (effect.getDescriptionId() == 'effect.cold_sweat.warmth' && effect.getAmplifier() >= 9) {
-            player.potionEffects.add('ars_nouveau:mana_regen', 3 * 20, 0, false, false);
+            player.potionEffects.add('ars_nouveau:mana_regen', 4 * 20, 0, false, false);
         }
     });
 });
