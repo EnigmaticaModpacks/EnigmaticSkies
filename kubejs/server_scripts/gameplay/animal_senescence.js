e3b016ffb1995f85960b9ec6a898474b7477a519
MuteTiefling <mutetiefling@gmail.com>
2026-05-18 08:50:25 -0400

    Attempt to reduce lag from senescence

--- kubejs/server_scripts/gameplay/animal_senescence.js
+++ kubejs/server_scripts/gameplay/animal_senescence.js
@@ -3,97 +3,118 @@ LevelEvents.tick((event) => {
 
     const ticks_per_day = 24000;
     const max_senescence = ticks_per_day * 10;
+    const execution_interval = 30 * 20;
+    const senescence_debug = true;
 
     const dimension = String(level.getDimension());
     const execute = `execute in ${dimension} run`;
 
-    // console.log(level.tickCount);
+    if (server.tickCount % execution_interval != 0) {
+        return;
+    }
 
     level.level.entities.forEach((entity) => {
         if (!entity.isLiving() || entity.isPlayer()) {
             return;
         }
 
-        let age = entity.nbt.Age;
-        let pData = entity.persistentData;
+        let entityData = entity.persistentData;
         let uuid = entity.uuid;
 
-        // let type = String(entity.entityType).split('.').slice(1).join(':');
-
         // Check if it matches our tag
-        let isSenescent = entity
-            .getEntityType()
-            .getTags()
-            .anyMatch((tag) => tag.location() == 'enigmatica:senescence');
+        if (!entityData.isSenescent) {
+            if (senescence_debug) console.log(`${entity.type} with UUID (${uuid}) isn't registered yet, adding!`);
+            let hasTag = entity
+                .getEntityType()
+                .getTags()
+                .anyMatch((tag) => tag.location() == 'enigmatica:senescence');
 
-        // Mark the mob for senescence system
-        if (isSenescent && age == 0 && !pData.getInt('senescence')) {
-            pData.putInt('senescence', 1);
-            pData.putInt('original_max_health', entity.getMaxHealth());
+            entityData.putBoolean('isSenescent', hasTag);
         }
 
-        if (pData.getInt('senescence')) {
-            let senescence = pData.getInt('senescence');
-            pData.putInt('senescence', senescence + 1);
+        // Mark the mob for senescence system
+        if (entityData.getBoolean('isSenescent') && !entityData.getInt('senescence')) {
+            if (entity.nbt.Age == 0) {
+                if (senescence_debug) {
+                    console.log(`${entity.type} with UUID (${uuid}) is old enough to start contributing!`);
+                }
+                entityData.putInt('senescence', execution_interval);
+                entityData.putInt('original_max_health', entity.getMaxHealth());
+            } else {
+                if (senescence_debug) {
+                    console.log(`${entity.type} with UUID (${uuid}) is too young to contribute!`);
+                }
+            }
+        }
 
-            // Only check if effects need to be applied once per minute
-            if (senescence % 1200 == 0) {
-                let original_max_health = pData.getInt('original_max_health');
-                let commands = [];
+        // Check mob's age to see if related effects need to be applied
+        if (entityData.getInt('senescence')) {
+            let senescence = entityData.getInt('senescence');
+            entityData.putInt('senescence', senescence + execution_interval);
 
-                if (senescence > max_senescence * 0.5) {
-                    // Reduce Health in middle age
-                    commands = commands.concat([
-                        `data merge entity ${uuid} {attributes:[{id:"minecraft:generic.max_health",base:${
-                            original_max_health * 0.5
-                        }}]}`
-                    ]);
+            if (senescence_debug) {
+                console.log(
+                    `${entity.type} with UUID (${uuid}) has been checked for advancing age. Current Age: ${senescence}`
+                );
+            }
+
+            let original_max_health = entityData.getInt('original_max_health');
+            let commands = [];
+
+            if (senescence > max_senescence * 0.5) {
+                // Reduce Health in middle age
+                commands = commands.concat([
+                    `data merge entity ${uuid} {attributes:[{id:"minecraft:generic.max_health",base:${
+                        original_max_health * 0.5
+                    }}]}`
+                ]);
+            }
+
+            if (senescence > max_senescence * 0.8) {
+                // Make elderly animals slow, blind, and less healthy
+                commands = commands.concat([
+                    `effect give ${uuid} minecraft:slowness infinite 0`,
+                    `effect give ${uuid} minecraft:blindness infinite 0`,
+                    `data merge entity ${uuid} {attributes:[{id:"minecraft:generic.max_health",base:${
+                        original_max_health * 0.2
+                    }}]}`
+                ]);
+            }
+
+            if (senescence > max_senescence) {
+                // Clear the loot table and ensure the creatures dies by various effects. This should convey better to the player what is happening.
+                if (senescence_debug) {
+                    console.log(
+                        `${entity.type} with UUID (${uuid}) has reached the end of it's life (Age: ${senescence}). Time to go.`
+                    );
                 }
 
-                if (senescence > max_senescence * 0.8) {
-                    // Make elderly animals slow, blind, and less healthy
-                    commands = commands.concat([
-                        `effect give ${uuid} minecraft:slowness infinite 0`,
-                        `effect give ${uuid} minecraft:blindness infinite 0`,
-                        `data merge entity ${uuid} {attributes:[{id:"minecraft:generic.max_health",base:${
-                            original_max_health * 0.2
-                        }}]}`
-                    ]);
-                }
+                commands = commands.concat([
+                    `data merge entity ${uuid} {DeathLootTable:"minecraft:empty"}`,
+                    `data merge entity ${uuid} {attributes:[{id:"minecraft:generic.max_absorption",base:0}]}`,
 
-                if (senescence > max_senescence) {
-                    // Clear the loot table and ensure the creatures dies by various effects. This should convey better to the player what is happening.
-                    // console.log(`${entity.type} has reached the end of it's life (Age: ${senescence}). Time to go.`);
-                    commands = commands.concat([
-                        `data merge entity ${uuid} {DeathLootTable:"minecraft:empty"}`,
-                        `data merge entity ${uuid} {attributes:[{id:"minecraft:generic.max_absorption",base:0}]}`,
+                    `effect give ${uuid} malum:grim_certainty infinite 9`,
+                    `effect give ${uuid} minecraft:wither infinite 9 true`,
+                    `effect give ${uuid} naturesaura:breathless infinite 9 true`,
+                    `effect give ${uuid} the_bumblezone:paralyzed infinite 9 true`,
+                    `effect give ${uuid} apothic_attributes:sundering infinite 9 true`,
+                    `effect give ${uuid} apothic_attributes:grievous infinite 9 true`,
+                    `effect clear ${uuid} minecraft:resistance`,
 
-                        `effect give ${uuid} malum:grim_certainty infinite 9`,
-                        `effect give ${uuid} minecraft:wither infinite 9 true`,
-                        `effect give ${uuid} naturesaura:breathless infinite 9 true`,
-                        `effect give ${uuid} the_bumblezone:paralyzed infinite 9 true`,
-                        `effect give ${uuid} apothic_attributes:sundering infinite 9 true`,
-                        `effect give ${uuid} apothic_attributes:grievous infinite 9 true`,
-                        `effect clear ${uuid} minecraft:resistance`,
+                    `particle minecraft:soul ${entity.x} ${entity.y + 2.5} ${entity.z}`
+                ]);
+            }
 
-                        `particle minecraft:soul ${entity.x} ${entity.y + 2.5} ${entity.z}`
-                    ]);
-                }
+            if (senescence > max_senescence * 1.5) {
+                // Extra damage if the mob is somehow kept alive this long
+                commands = commands.concat([`effect give ${uuid} minecraft:instant_damage infinite 99 true`]);
+            }
 
-                if (senescence > max_senescence * 1.5) {
-                    // Extra damage if the mob is somehow kept alive this long
-                    commands = commands.concat([`effect give ${uuid} minecraft:instant_damage infinite 99 true`]);
-                }
-
-                commands.forEach((command) => {
+            commands.forEach((command) => {
+                server.scheduleInTicks(2, () => {
                     server.runCommandSilent(`${execute} ${command}`);
                 });
-
-                // Possibly causing a conflict with Cold Sweat
-                // server.scheduleInTicks(2, () => {
-                //     entity.discard();
-                // });
-            }
+            });
         }
     });
 });
